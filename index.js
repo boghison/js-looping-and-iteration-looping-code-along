c166036306494162f911204702f6400893505487
Bogdan Cuza <bogdancuza@protonmail.com>
2022-07-12 16:49:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let greetingArray = [];
+    for (let i = 0; i < names.length; i++) {
+        greetingArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return greetingArray;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
